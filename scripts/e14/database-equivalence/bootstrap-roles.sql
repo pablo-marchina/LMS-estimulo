82acd9cc5f030f0303452a6fdc5f98575d543216
Pablo Marchina <pablomarchina@gmail.com>
2026-07-10 00:44:19 -0300

    test(database): avoid protected role alterations

--- scripts/e14/database-equivalence/bootstrap-roles.sql
+++ scripts/e14/database-equivalence/bootstrap-roles.sql
@@ -5,17 +5,13 @@
 do $$
 begin
   if not exists (select 1 from pg_roles where rolname = 'app_readonly') then
-    create role app_readonly nologin inherit;
+    create role app_readonly nologin inherit connection limit -1;
   end if;
   if not exists (select 1 from pg_roles where rolname = 'app_runtime') then
-    create role app_runtime nologin inherit;
+    create role app_runtime nologin inherit connection limit -1;
   end if;
   if not exists (select 1 from pg_roles where rolname = 'app_worker') then
-    create role app_worker nologin inherit;
+    create role app_worker nologin inherit connection limit -1;
   end if;
 end
 $$;
-
-alter role app_readonly nologin inherit nosuperuser nocreatedb nocreaterole noreplication nobypassrls connection limit -1;
-alter role app_runtime nologin inherit nosuperuser nocreatedb nocreaterole noreplication nobypassrls connection limit -1;
-alter role app_worker nologin inherit nosuperuser nocreatedb nocreaterole noreplication nobypassrls connection limit -1;
