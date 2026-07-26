2e006091f6b98970ae2a4de1a72c549fed81d126
Pablo Marchina <pablomarchina@gmail.com>
2026-07-26 19:57:31 -0300

    security: require gateway for participant journey listing

---
+++ supabase/migrations/20260726233006_lock_participant_journey_listing_rpc.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.e14_list_participant_journeys(uuid) from public,anon,authenticated;
+grant execute on function public.e14_list_participant_journeys(uuid) to service_role;
